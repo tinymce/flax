bfab48bd818141ab99cb98b02d7662c703fb729e
techtangents <dylan@techtangents.com>
2018-04-09 21:55:10 +1000

    Test

--- src/test/scala/com/ephox/flax/tests/ActionTest.scala
+++ src/test/scala/com/ephox/flax/tests/ActionTest.scala
@@ -1,15 +1,22 @@
 package com.ephox.flax
 package tests
 
+import com.ephox.flax.api.action.{Action, Err, Log}, Log._, Err._
 import com.ephox.flax.api.action.Action.point
 import org.specs2.ScalaCheck
 import org.specs2.mutable.Specification
 import com.ephox.flax.misc.TestUtils._
 import org.specs2.scalaz.ScalazMatchers
-import scalaz.\/-
+import scalaz._
+import scalaz.std.anyVal._
+import scalaz.std.string._
+import scalaz.std.tuple._
 import scalaz.syntax.monad._
 
 class ActionTest extends Specification with ScalaCheck with ScalazMatchers {
+
+  def ⊥[A]: A = sys.error("⊥")
+
   "Action" should {
     "anon bind" in prop { (i: Int, j: Int) =>
       runAndGetValue(point(i) >> point(j)) must_=== \/-(j)
@@ -17,6 +24,12 @@ class ActionTest extends Specification with ScalaCheck with ScalazMatchers {
 
     "bind" in prop { (i: Int, j: Int) =>
       runAndGetValue(point(i) >>= (i => point((i, j)))) must_=== \/-((i, j))
+    }
+
+    "fromSideEffectWithLog" >> {
+      "with pure value" >> prop { (s: String, i: Int) =>
+        Action.fromSideEffectWithLog(s, _ => i).run.run.run.run(null).unsafePerformIO() must equal((Log.single(s), \/.right[Err, Int](i)))
+      }
     }
   }
 }
