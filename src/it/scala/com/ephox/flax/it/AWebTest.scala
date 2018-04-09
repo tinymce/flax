698d91df7937bc6ede04d300c6f5fe7aee720cb6
techtangents <dylan@techtangents.com>
2018-04-09 22:35:59 +1000

    Testing flax github

--- src/it/scala/com/ephox/flax/it/AWebTest.scala
+++ src/it/scala/com/ephox/flax/it/AWebTest.scala
@@ -1,15 +1,23 @@
 package com.ephox.flax.it
 
-import com.ephox.flax.api.action.FlaxActions
+import com.ephox.flax.api.action.Action, Action._
+import com.ephox.flax.api.action.FlaxActions._
 import com.ephox.flax.api.elem.Browser
 import com.ephox.flax.api.specs2.FlaxSpec
+import org.openqa.selenium.By.{linkText, partialLinkText}
 import org.specs2.mutable.Specification
+import scalaz.syntax.monad._
 
 class AWebTest extends Specification with FlaxSpec {
 
   override def curBrowser: Browser = Browser.firefox
 
-  "flaxo" >> {
-    FlaxActions.get("https://tinymce.com")
+  "Flax github" >> {
+    for {
+      _ <- get("https://github.com/ephox/flax/")
+      _ <- clickBy(linkText("geckodriver"))
+      _ <- nested("silly navigations", back *> forward *> back)
+      _ <- clickBy(partialLinkText("ExampleTestBase"))
+    } yield ()
   }
 }
